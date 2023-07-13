a48eaa267d72df2408b911ab1cf93d433418191b
unknown <Roger.PenhaSenai@Gmail.com>
2023-07-13 18:50:42 -0300

    calculadora finalizada

---
+++ node.js
@@ -0,0 +1,23 @@
+function inserir(num){
+    var número = document.getElementById('tela').innerHTML;
+    document.getElementById('tela').innerHTML = número + num
+}
+function limpar(){
+    document.getElementById('tela').innerHTML = ''
+}
+
+function apagar(){
+    var resultado = document.getElementById('tela').innerHTML;
+    resultado = document.getElementById('tela').innerHTML = resultado.substring(0, resultado.length - 1);
+}
+
+function calcular(){
+    var resultado = document.getElementById('tela').innerHTML;
+    if (resultado){
+        document.getElementById('tela').innerHTML = eval(resultado);
+    }
+}
+
+function printar(){
+    window.print()
+}
